f0d510ebbc434b1f81d47a8e7b8fca0fda28a602
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-08 11:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+// const ropeLengths = document.getElementById("rope-lengths");
+// function calculateMinCost() {
+// 	const ropes = ropeLengths.value.split(',').map(Number);
+// 	const priorityQueue=[];
+// 	ropes.forEach(length=>priorityQueue.push(length));
+// 	let totalCost=0;
+// 	while(priorityQueue.length>1){
+// 		const smallest1=priorityQueue.shift();
+// 		const smallest2=priorityQueue.shift();
+// 		const cost=smallest1+smallest2;
+// 		totalCost+=cost;
+// 		priorityQueue.push(cost);
+// 		priorityQueue.sort((a,b)=> a-b);
+// 	}
+//   document.getElementById("result").innerHTML=totalCost;
+// }  
 const ropeLengths = document.getElementById("rope-lengths");
 function calculateMinCost() {
-	const ropes = ropeLengths.value.split(',').map(Number);
-	const priorityQueue=[];
-	ropes.forEach(length=>priorityQueue.push(length));
-	let totalCost=0;
-	while(priorityQueue.length>1){
-		const smallest1=priorityQueue.shift();
-		const smallest2=priorityQueue.shift();
-		const cost=smallest1+smallest2;
-		totalCost+=cost;
+   const ropes = ropeLengths.value.split(',').map(Number);
+	
+	const priorityQueue = [];
+	ropes.forEach(length => priorityQueue.push(length));
+	
+	let totalCost = 0;
+	while(priorityQueue.length > 1){
+		const smallest1 = priorityQueue.shift();
+		const smallest2 = priorityQueue.shift();
+		const cost = smallest1 + smallest2;
+		totalCost += cost;
 		priorityQueue.push(cost);
-		priorityQueue.sort((a,b)=> a-b);
+		priorityQueue.sort((a, b) => a - b);
 	}
-  document.getElementById("result").innerHTML=totalCost;
-}  
+	document.getElementById("result").innerHTML = totalCost;
+}
   
