f0270fb62a2acd5e5c6bce550976e0ba874affd7
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-08 11:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+const ropeLengths=document.getElementById("rope-lengths");
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+	const ropes= ropeLengths.value.split(',').map(Number);
+	const priorityQueue=[];
+	ropes.forEach(length=>priorityQueue.push(length));
+	let totalCost=0;
+	while(priorityQueue.length>1){
+		const smallest1=priorityQueue.shift();
+		const smallest2=priorityQueue.shift();
+		const cost=smallest1+smallest2;
+		totalCost+=cost;
+		priorityQueue.push(cost);
+		priorityQueue.sort((a,b)=> a-b);
+	}
+  document.getElementById("result").innerHTML=totalCost;
 }  
+  
